e3db6b04909ca7115b247b99309e557e97fb874d
Michael Fleming <flemin100@gmail.com>
2016-11-04 14:48:42 +0000

    Background Image

--- webapp.py
+++ webapp.py
@@ -10,5 +10,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/login')
+def login():
+
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run()
