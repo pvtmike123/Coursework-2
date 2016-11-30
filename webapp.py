8350f7704ce8caf9ea85037407ca39a3947493c9
Michael Fleming <flemin100@gmail.com>
2016-11-30 12:00:05 +0000

    Admin edit blog details
    Admin users now able to edit and delete blog details

--- webapp.py
+++ webapp.py
@@ -3,11 +3,9 @@ from contextlib import closing
 import sqlite3 as lite
 import sys
 from flask import Flask, render_template, request, Response, session, g, redirect, url_for, \
-     abort, flash
-
+    abort, flash
 
 DATABASE = '/Users/flemin100/Documents/Uni/AWT/Coursework-2/webapp/sales.db'
-
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -21,7 +19,7 @@ def connect_db():
 @app.route('/index')
 def index():
     g.db = connect_db()
-    cur = g.db.execute('select title, author, id, content, image from content LIMIT 6')
+    cur = g.db.execute('SELECT title, author, id, content, image FROM content LIMIT 6')
     sales = [dict(title=row[0], author=row[1], id=row[2], content=row[3], image=row[4]) for row in cur.fetchall()]
     g.db.close()
     return render_template("index.html", sales=sales)
@@ -33,12 +31,14 @@ def check_auth(username, password):
     """
     return username == 'admin' and password == 'secret'
 
+
 def authenticate():
     """Sends a 401 response that enables basic auth"""
     return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
@@ -47,6 +47,7 @@ def requires_auth(f):
         if not auth or not check_auth(auth.username, auth.password):
             return authenticate()
         return f(*args, **kwargs)
+
     return decorated
 
 
@@ -54,7 +55,7 @@ def requires_auth(f):
 @requires_auth
 def secret_page():
     g.db = connect_db()
-    cur = g.db.execute('select title, author, id, content from content')
+    cur = g.db.execute('SELECT title, author, id, content FROM content')
     sales = [dict(title=row[0], author=row[1], id=row[2], content=row[3]) for row in cur.fetchall()]
     g.db.close()
     return render_template('admin/admin.html', sales=sales)
@@ -64,7 +65,7 @@ def secret_page():
 @requires_auth
 def posts():
     g.db = connect_db()
-    cur = g.db.execute('select title, author, id, content from content')
+    cur = g.db.execute('SELECT title, author, id, content FROM content')
     sales = [dict(title=row[0], author=row[1], id=row[2], content=row[3]) for row in cur.fetchall()]
     g.db.close()
     return render_template('admin/admin_blog_posts.html', sales=sales)
@@ -74,8 +75,9 @@ def posts():
 @requires_auth
 def my_details():
     g.db = connect_db()
-    cur = g.db.execute('select id, first_name, second_name, email, dob, img from users')
-    users = [dict(id=row[0], first_name=row[1], second_name=row[2], email=row[3], dob=row[4], img=row[5]) for row in cur.fetchall()]
+    cur = g.db.execute('SELECT id, first_name, second_name, email, dob, img FROM users')
+    users = [dict(id=row[0], first_name=row[1], second_name=row[2], email=row[3], dob=row[4], img=row[5]) for row in
+             cur.fetchall()]
     g.db.close()
     return render_template('admin/my_details.html', users=users)
 
@@ -83,23 +85,78 @@ def my_details():
 @app.route('/blog/<id>')
 def blog(id=id):
     g.db = connect_db()
-    id=id
-    cur = g.db.execute('select title, author, id, image, content, date from content WHERE id=?', [id])
-    sales = [dict(title=row[0], author=row[1], id=row[2], image=row[3], content=row[4], date=row[5]) for row in cur.fetchall()]
+    cur = g.db.execute('SELECT title, author, id, image, content, date FROM content WHERE id=?', [id])
+    sales = [dict(title=row[0], author=row[1], id=row[2], image=row[3], content=row[4], date=row[5]) for row in
+             cur.fetchall()]
     g.db.close()
     return render_template("blog_post.html", sales=sales)
 
 
+@app.route('/edit/<id>')
+@requires_auth
+def editblog(id=id):
+    g.db = connect_db()
+    cur = g.db.execute('SELECT title, author, id, image, content, date FROM content WHERE id=?', [id])
+    sales = [dict(title=row[0], author=row[1], id=row[2], image=row[3], content=row[4], date=row[5]) for row in
+             cur.fetchall()]
+    g.db.close()
+    return render_template("admin/edit_blog_post.html", sales=sales)
+
+
+@app.route('/updateblog', methods=['POST', 'GET'])
+def updateblog():
+    if request.method == 'POST':
+        try:
+            title = request.form['title']
+            author = request.form['author']
+            date = request.form['date']
+            content = request.form['content']
+
+            with g.connect("sales.db") as con:
+                g.db = connect_db()
+                cur = con.cursor()
+                cur.execute("INSERT INTO content (title,author,date,content) VALUES(?, ?, ?, ?)",
+                            (title, author, date, content))
+
+                cur.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+        finally:
+            return render_template("result.html")
+            g.db.close()
+
+
+@app.route('/update-blog-content/<id>', methods=['POST'])
+@requires_auth
+def editblogcontent(id=id):
+    g.db = connect_db()
+    cur = g.db.execute("INSERT INTO content (title,author,date,content) VALUES (?,?,?,?)",
+                       (title, author, date, content))
+    sales = [dict(title=row[0], author=row[1], id=row[2], image=row[3], content=row[4], date=row[5]) for row in
+             cur.fetchall()]
+    g.db.close()
+
+
+@app.route('/delete-blog/<id>')
+def deleteblog(id=id):
+    g.db = connect_db()
+    g.db.execute('DELETE FROM content WHERE id=?', [id])
+    g.db.close()
+    redirect(url_for('secret_page'))
+
+
 @app.route('/about')
 def about_me():
-
     return render_template("about.html")
 
 
 @app.route('/blogs-all')
 def allblogs():
     g.db = connect_db()
-    cur = g.db.execute('select title, author, id, content, image from content')
+    cur = g.db.execute('SELECT title, author, id, content, image FROM content')
     sales = [dict(title=row[0], author=row[1], id=row[2], content=row[3], image=row[4]) for row in cur.fetchall()]
     g.db.close()
     return render_template("blogs_all.html", sales=sales)
